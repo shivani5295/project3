2609a20ac51752d57d02c4a1297eeb2a26627882
delta student <shivaninigam5295@gmail.com>
2025-08-28 09:12:24 +0530

    Added button code in feature branch

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 This is a project created from the local system.
 
 Created by **Shivani**.
+This branch contains a **button** implementation.
--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Welcome to Shivani's project!");
+document.getElementById("magicBtn").addEventListener("click", function() {
+  alert("Button clicked from FEATURE branch!");
+});
